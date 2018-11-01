5d3c4e9156dfb6e54c61545085c104203a2b9c1b
Jacob Schloss <jschloss@swiftengineering.com>
2018-11-01 12:02:45 -0700

    Merge pull request #22 in EL_AI/swift_common from Gaussian_Markov_random_walk to master
    Squashed commit of the following:
    
    commit 95f7e444d0dcecc4adc3d23e04d09f7d5719e472
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Thu Nov 1 12:00:36 2018 -0700
    
        add init function
    
    commit 9d1ac77731f3948b8b67b6ea953ab6a1f9343789
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Thu Nov 1 11:59:22 2018 -0700
    
        make shared init function
    
    commit cad2b0cbc07edb8fc7b125ca454ca93ae7526e95
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Tue Oct 30 16:55:23 2018 -0700
    
        docs
    
    commit 0fe50b519d68224f5c4c05ad6ba2f7ccfc9e793c
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Tue Oct 30 16:31:29 2018 -0700
    
        fix resetting state
    
    commit 0a95a50153f8165529f3e5749f5f4338be545c7e
    Merge: 322fe41 00e8344
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Tue Oct 30 15:33:43 2018 -0700
    
        merge
    
    commit 322fe41e467930d05e2069fe94b0498d470c8acb
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Tue Oct 30 15:33:10 2018 -0700
    
        add init value
    
    commit 83c398cdac8499262e73c5e03a46b6721e3749c5
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Tue Oct 30 15:31:26 2018 -0700
    
        fix
    
    commit 527d9602159f8abdee03077d28f158db394e39f9
    Author: Jacob Schloss <jschloss@swiftengineering.com>
    Date:   Wed Oct 24 17:34:45 2018 -0700
    
        add markov noise

---
+++ include/swift_common/GaussianMarkov_noise.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <random>
+#include <sstream>
+
+class GaussianMarkov_noise
+{
+public:
+
+	///
+	/// Init class
+	/// Picks a random rng seed from a random device
+	/// \param  [in] tau - GM time constant, in seconds
+	/// \param  [in] sigma - GM std dev
+	/// \param  [in] initial_output - GM inital value
+	GaussianMarkov_noise(const double tau, const double sigma, const double initial_output);
+	
+	///
+	/// Init class
+	/// \param  [in] tau - GM time constant, in seconds
+	/// \param  [in] sigma - GM std dev
+	/// \param  [in] initial_output - GM inital value
+	/// \param  [in] seed - RNG seed
+	/// \param  [in] seed_len - Length of RNG seed
+	///
+	GaussianMarkov_noise(const double tau, const double sigma, const double initial_output, const uint32_t seed[], const size_t seed_len);
+
+	///
+	/// Reset distribution and RNG to initial state
+	///
+	void reset();
+
+	///
+	/// Time since reset
+	/// \return return next value in sequence
+	///
+	double update(const double dT);
+
+protected:
+
+	void initialize(const double tau, const double sigma, const double initial_output, const uint32_t seed[], const size_t seed_len);
+
+	double m_tau;
+	double m_sigma;
+	double m_initial_output;
+
+	std::mt19937_64 m_rand_gen;
+	std::normal_distribution<double> m_normal_dist;
+
+	double m_last_output;
+
+	std::stringstream m_init_rng_state;
+};
---
+++ src/GaussianMarkov_noise.cpp
@@ -0,0 +1,61 @@
+#include "swift_common/GaussianMarkov_noise.hpp"
+
+#include <algorithm>
+#include <functional>
+
+GaussianMarkov_noise::GaussianMarkov_noise(const double tau, const double sigma, const double initial_output)
+{
+	//arbitrarily pick 16 as length of seed
+	//no way to gaurntee mt19937 state is filled, so no point giving 624 elements
+	std::vector<std::uint32_t> seed_data(16);
+
+	std::random_device rd;
+	std::generate_n(seed_data.data(), seed_data.size(), std::ref(rd));
+
+	//delegate initialization
+	initialize(tau, sigma, initial_output, seed_data.data(), seed_data.size());
+}
+
+GaussianMarkov_noise::GaussianMarkov_noise(const double tau, const double sigma, const double initial_output, const uint32_t seed[], const size_t seed_len)
+{
+	//delegate initialization
+	initialize(tau, sigma, initial_output, seed, seed_len);
+}
+
+void GaussianMarkov_noise::initialize(const double tau, const double sigma, const double initial_output, const uint32_t seed[], const size_t seed_len)
+{
+	m_tau = tau;
+	m_sigma = sigma;
+
+	m_initial_output = initial_output;
+	m_last_output = m_initial_output;
+
+	m_normal_dist = std::normal_distribution<double>(0.0, 1.0);
+
+	//init rng
+	std::seed_seq seed_gen(seed, seed + seed_len);
+	m_rand_gen.seed(seed_gen);
+
+	//save initial state
+	m_init_rng_state << m_rand_gen;	
+}
+
+void GaussianMarkov_noise::reset()
+{
+	m_last_output = m_initial_output;
+
+	//reset the rng
+	m_init_rng_state >> m_rand_gen;
+	m_normal_dist.reset();
+}
+
+double GaussianMarkov_noise::update(const double dT)
+{
+	const double alpha = exp(-dT / m_tau);
+
+	const double y_n = alpha * m_last_output + (1.0 - alpha) * m_sigma * m_normal_dist(m_rand_gen);
+
+	m_last_output = y_n;
+
+	return y_n;
+}
