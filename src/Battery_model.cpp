e823b7a0dc4acb9052a82e378f92916d739cd027
Richard Kirby <richard@kspresearch.com>
2018-11-28 11:29:14 -0800

    Merge pull request #26 in EL_AI/swift_common from battery_model to master
    Squashed commit of the following:
    
    commit 21a2eebc2efc947b38e05fdf0d61cd577bfc3f8f
    Merge: 39e2fbc bce989c
    Author: Parallels <richard@kspresearch.com>
    Date:   Wed Nov 28 11:04:46 2018 -0700
    
        Fixed merge conflicts.
    
    commit 39e2fbca2eea2903b12b4e650498021a8340cb0d
    Author: Parallels <richard@kspresearch.com>
    Date:   Wed Nov 28 10:39:49 2018 -0700
    
        Put coefficients in an array inside the discharge curve struct.
    
    commit bce989c0a91f6e02b84120ca67112b57dd714474
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 27 11:04:07 2018 -0700
    
        Changed add discharge method to use const reference.
    
    commit c51cfb6efe4831e016d0bdd2574532ecb9cbc2b5
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 27 10:54:44 2018 -0700
    
        made constructor equal default.
    
    commit d56cc4000b6c6c7eb797ea8283a2b506a9089392
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 27 10:47:03 2018 -0700
    
        Replaced std:array with std:vector for discharge curves.
    
    commit c467376f75b2d74d22c0dc73f2cb12360b0a2827
    Merge: dc99dcb 575a6e7
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 27 10:17:28 2018 -0700
    
        Fixed merge conflicts.
    
    commit dc99dcb09770c60f61faed6c6536a32b7b3e2695
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 27 09:58:16 2018 -0700
    
        Changed some function parameters to *const, changed to cstdio, fixed white space issues.
    
    commit 575a6e78581eac24a5fc4c1223dc256376dff30a
    Author: Parallels <richard@kspresearch.com>
    Date:   Wed Nov 21 08:36:57 2018 -0700
    
        Added missing doxygen comments.
    
    commit 8e6a98f19abe6ce3dcb5c2e21cd441ab989bad91
    Author: Parallels <richard@kspresearch.com>
    Date:   Wed Nov 21 07:17:18 2018 -0700
    
        Refactoring.
    
    commit 0ba69e104532499fe7be9337c13911005bedfa58
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 20 16:39:26 2018 -0700
    
        Added range checking.
    
    commit a011d5dbb7cd9371f46efa421497d669b3ffe9c9
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 20 14:56:50 2018 -0700
    
        Refactored and removed debugging statements.
    
    commit 6b74d40fcac3b61093b2638d5f2ed2415b05f7cb
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 20 08:56:15 2018 -0700
    
        Updated path.
    
    commit 46475a27080253fc75342fc5eecfdf84df40217d
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 20 08:55:20 2018 -0700
    
        Added Battery_model
    
    commit 3068b75031034cb161052aaa3914659ea7d1dd26
    Author: Richard <richard@kspresearch.com>
    Date:   Tue Nov 20 08:30:09 2018 -0700
    
        Commented out debug statements.
    
    commit d6367757026cbd07a49a08e60922a60e6b45ab56
    Author: Parallels <richard@kspresearch.com>
    Date:   Mon Nov 19 13:50:04 2018 -0700
    
        Added transient response, fixed interpolation bug.
    
    commit c2b592eece0f1477228ace8bc1967a26b5f43a58
    Author: Richard <richard@kspresearch.com>
    Date:   Sat Nov 17 16:51:26 2018 -0700
    
        Initial commit.

---
+++ src/Battery_model.cpp
@@ -0,0 +1,132 @@
+#include "swift_common/Battery_model.hpp"
+#include <cmath>
+#include <cstdio>
+#include <limits>
+#include <cstdint>
+
+bool Battery_model::initialize(const double initial_soc, const double capacity, const uint8_t num_cells, const double C, const double R)
+{
+    if (R <= 0.0 || C < 0.0)
+        return false;		// R must be > 0 or divide by zero error, set C = 0 to turn off transient response.
+    m_num_discharge_curves = 0;
+    m_initial_soc = initial_soc;
+    m_soc = m_initial_soc;		// in Ah
+    m_capacity = capacity;		// in Ah
+    m_num_cells = num_cells;
+    m_last_voltage = 4.2;
+    m_c = C;
+    m_r = R;
+    m_low_current_curve_current = std::numeric_limits<double>::infinity();
+    m_high_current_curve_current = -1;
+    return true;
+}
+
+void Battery_model::update_soc(const double current, const double timestep, double* const voltage, double* const soc)
+{
+    // simple coluomb counter
+    if (timestep > 0.0)
+    {
+        m_soc += current * timestep/3600;				// Time step is in seconds, need hours for mAhrs
+        *voltage = get_voltage(m_soc * 1000, current, timestep);	// curves are in mA hours
+        *soc = m_soc;
+    }
+    else
+    {
+        *voltage = m_last_voltage;
+        *soc = m_soc;
+    }
+}
+
+// discharge curves are in mAhrs 
+double Battery_model::get_voltage(const double soc, const double current_in, const double timestep)
+{
+    double current = current_in;
+
+    // Ensure that there are at least two discharge curves
+    if(m_num_discharge_curves < 2)
+        return m_num_cells*m_last_voltage;
+
+    // Ensure current is between the two discharge curves
+    if(current > m_high_current_curve_current)
+        current = m_high_current_curve_current;
+    if(current < m_low_current_curve_current)
+        current = m_low_current_curve_current;
+
+    // Find the curves that bracket the load
+    int8_t low_current_discharge_curve = -1, high_current_discharge_curve = -1;
+    double delta1 = std::numeric_limits<double>::infinity(), delta2 = std::numeric_limits<double>::infinity();
+    for (uint8_t i = 0 ; i < m_num_discharge_curves ; i++)
+    {
+        if (m_discharge_curves[i].load <= current)
+        {
+            if (delta1 > (current - m_discharge_curves[i].load))
+            {
+                delta1 = current - m_discharge_curves[i].load;
+                low_current_discharge_curve = i;
+            }
+        }
+
+        if (m_discharge_curves[i].load >= current)
+        {
+            if (delta2 > (m_discharge_curves[i].load - current))
+            {
+                delta2 = m_discharge_curves[i].load - current;
+                high_current_discharge_curve = i;
+            }
+        }
+    }
+
+    // The low voltage comes from the high discharge current and vice versa
+    double voltage_low = compute_voltage(soc, high_current_discharge_curve);
+    double voltage_high = compute_voltage(soc, low_current_discharge_curve);
+
+    // If the load is bracketed by the same curve, then the load is equal to that curve otherwise interpolate
+    double voltage;
+    if(low_current_discharge_curve == high_current_discharge_curve)
+        voltage = voltage_low;
+    else
+        voltage = ((current - m_discharge_curves[low_current_discharge_curve].load)/
+            ( m_discharge_curves[high_current_discharge_curve].load - m_discharge_curves[low_current_discharge_curve].load))
+            * (voltage_high - voltage_low) + voltage_low;
+
+    // add transient response
+    voltage = voltage - (m_c/m_r) * (voltage - m_last_voltage)/(timestep);
+    m_last_voltage = voltage;
+
+    return m_num_cells*voltage;
+}
+
+bool Battery_model::add_discharge_curves(const discharge_curve& dc)
+{
+    if (m_num_discharge_curves > max_num_discharge_curves)
+        return false;
+    m_discharge_curves.push_back(dc);
+
+    if (m_discharge_curves[m_num_discharge_curves].load < m_low_current_curve_current)
+    {
+        m_low_current_curve_current = m_discharge_curves[m_num_discharge_curves].load;
+        m_index_of_low_current_curve = m_num_discharge_curves;
+    }
+
+    if (m_discharge_curves[m_num_discharge_curves].load > m_high_current_curve_current)
+    {
+        m_high_current_curve_current = m_discharge_curves[m_num_discharge_curves].load;
+        m_index_of_high_current_curve = m_num_discharge_curves;
+    }
+
+    m_num_discharge_curves++;
+    return true;
+}
+
+double  Battery_model::compute_voltage(const double soc, const uint8_t curve)
+{
+    double voltage = m_discharge_curves[curve].c[0]
+        + m_discharge_curves[curve].c[1] * soc
+        + m_discharge_curves[curve].c[2] * pow(soc,2)
+        + m_discharge_curves[curve].c[3] * pow(soc,3)
+        + m_discharge_curves[curve].c[4] * pow(soc,4)
+        + m_discharge_curves[curve].c[5] * pow(soc,5);
+
+    return voltage;
+}
+
