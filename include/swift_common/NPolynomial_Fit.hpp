f211f09c101891dab1ebecc0426c8d676a028ba9
Jacob Schloss <jschloss@swiftengineering.com>
2018-12-07 14:32:26 -0800

    Merge pull request #30 in EL_AI/swift_common from pr-add_NPolynomialFit_template_class to master
    Squashed commit of the following:
    
    commit 192e7b5ad182582909fb8ae4d2fcf2b71a5fbed8
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 22:22:45 2018 +0000
    
        NPolynomial_Fit: add function that has as argument the array of invariants
    
    commit 618b22e563b2dc1dc8ea552bfc6c053f26245644
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 21:47:34 2018 +0000
    
        NPolynomial_Fit: bring definitions to hpp file
    
    commit 28cec12715e7b9430eee3c1ae873d7ac883a1ca2
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 19:34:20 2018 +0000
    
        NPolynomial_Fit: delete default constructor and destructor as they are not used
    
    commit ef9c69f69d4500da470a2b0a74d163606d1d7524
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 19:32:50 2018 +0000
    
        NPolynomial_Fit: use #pragma once instead of include guards
    
    commit 938984862f7d073fc519438c6e72276a1cef67b9
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 19:31:31 2018 +0000
    
        NPolynomial_Fit: move template specialization to header file
    
    commit 5cd04cce67126614830ecd3b7919765ea398e0a3
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 19:21:16 2018 +0000
    
        fix number of invariants for 3rd degree poly
    
    commit 4fbfb9a8a72ca33a5dc8ad0b29ba02f014fb5ca3
    Author: TSC21 <n.marques21@hotmail.com>
    Date:   Thu Dec 6 16:14:33 2018 +0000
    
        adds NPolynomialFit class and drops PolyFit3degree

---
+++ include/swift_common/NPolynomial_Fit.hpp
@@ -0,0 +1,37 @@
+/**
+ * @brief       NPolynomialFit class
+ * @file        NPolynomial_Fit.hpp
+ * @author      Nuno Marques <nuno.marques@dronesolutions.io>
+ * @copyright   Copyright(C) 2018, Swift Engineering Inc. All rights reserved.
+ */
+
+#pragma once
+
+#include <boost/array.hpp>
+#include <vector>
+#include <boost/math/tools/rational.hpp>
+
+/// \brief NPolynomialFit class that implements a N-degree polynomial fit
+template <class T, size_t n>
+class NPolynomialFit {
+public:
+	/// \brief Gets the output from an input value and the poly indeterminates kept on the m_fit_curves set
+	T get_output_from_poly(const size_t &poly_index, const double &input) {
+		BOOST_ASSERT_MSG(poly_index <= (m_fit_curves.size() - 1), "The chosen polynomial curve does not exist!");
+		return boost::math::tools::evaluate_polynomial(m_fit_curves[poly_index], input);
+	}
+
+	/// \brief Gets the output from an input value and input poly indeterminates
+	T get_output_from_poly(const boost::array<T, n> &poly, const double &input) {
+		return boost::math::tools::evaluate_polynomial(poly, input);
+	}
+
+	/// \brief Loads a polynomial fit curve to be used
+	void load_poly_fit_curve(const boost::array<T, n> &poly) {
+		m_fit_curves.push_back(poly);
+	}
+
+protected:
+	/// \brief Array of polynomial fit curves
+	std::vector<boost::array<T, n> > m_fit_curves;
+};	// class NPolynomialFit
